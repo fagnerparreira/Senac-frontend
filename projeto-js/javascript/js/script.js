f8387c07fac2886237c7f11f40827378f8d7871a
Fagner Parreira <fagnerparreira1@gmail.com>
2021-07-19 14:33:10 -0300

    19/07
    exerc. senac

---
+++ projeto-js/javascript/js/script.js
@@ -0,0 +1,17 @@
+// alert("E ai blz?")
+
+
+
+var hora = new Date().getHours()
+var saudacao
+  if(hora<12) {
+    saudacao = "Bom dia!"
+  }
+  else if(hora<18) {
+    saudacao = "Boa Tarde!"
+  }
+  else{
+    saudacao = "Boa Noite!" 
+  }
+
+document.getElementById("mensagem").innerHTML = saudacao 
